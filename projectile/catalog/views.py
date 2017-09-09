1dd262a8163d594fd0b3ef36010e34a8690ff69f
kayes-shawon <shawon.kayes@gmail.com>
2017-09-10 02:20:29 +0600

    Added post method with authentication

--- projectile/catalog/views.py
+++ projectile/catalog/views.py
@@ -1,10 +1,17 @@
+from .permissions import IsAdminOrReadOnly
 from django.http import HttpResponse
 from rest_framework import generics
 from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
+from rest_framework import status
 
 
 class ProductList(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+class ProductList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = (IsAdminOrReadOnly, )
